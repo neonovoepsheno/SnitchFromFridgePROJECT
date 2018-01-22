9b1783183e36a791ba35367e0a516f25829aa975
k.yakupova <yakupova.k.v@yandex.by>
2018-01-23 00:08:02 +0300

    + add generation of several objects of the same type

--- Assets/Scripts/Gameplay/Food/FoodController.cs
+++ Assets/Scripts/Gameplay/Food/FoodController.cs
@@ -18,31 +18,33 @@ public class FoodController : MonoBehaviour
         start_time += Time.deltaTime;
     }
 
+
+
     public void OnMouseDown()
     {
         this.gameObject.SetActive(false);
         string name = this.gameObject.name;
-        if (name == "apple_ok")
+        if (name == "apple_ok(Clone)")
             SaturationBarController.currValue += 2 / coef;
-        else if (name == "apple_rot")
+        else if (name == "apple_rot(Clone)")
         {
             start_time = 0;
             time_of_action += 1.2f;
             coef = 2;
             SaturationBarController.currValue += 2 * coef_for_rot;
         }
-        else if (name == "burger_ok")
+        else if (name == "burger_ok(Clone)")
             SaturationBarController.currValue += 35 / coef;
-        else if (name == "burger_rot")
+        else if (name == "burger_rot(Clone)")
         {
             start_time = 0;
             time_of_action += 3.5f;
             coef = 2;
             SaturationBarController.currValue += 35 * coef_for_rot;
         }
-        else if (name == "meat_ok")
+        else if (name == "meat_ok(Clone)")
             SaturationBarController.currValue += 20 / coef;
-        else if (name == "meat_rot")
+        else if (name == "meat_rot(Clone)")
         {
             start_time = 0;
             time_of_action += 2.7f;
--- Assets/Scripts/Gameplay/FoodControl.cs
+++ Assets/Scripts/Gameplay/FoodControl.cs
@@ -2,35 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//добавь rigidbody, чтобы они падали
+
 public class FoodControl : MonoBehaviour {
 
-    public static string[] food = { "apple_ok", "burger_ok", "meat_ok"};
+    //public static string[] food = { "apple_ok", "burger_ok", "meat_ok"};
+    public GameObject[] food;
 
-    private static float[] y = { 26f, 14.5f, -2.5f, -14.3f, -27.7f };
-    private static float[] x = { -15.48f, -0.4332199f};
+    private static List<GameObject> generatedFood = new List<GameObject>();
+    private static float[] y = { 6f, 3.65f, -1.96f, 14.6f, -4.66f };
+    private static float[] x = { -3f, 0f};
+    private static GameObject[] foodPrefab;
 
-    public static void FoodGenerate_kill_me_please()
+    private void Start()
     {
-        for (int i = 0; i < food.Length; i++)
+      foodPrefab = food;
+    }
+
+    public static void FoodGenerate()
+    {
+        for (int i = 0; i < foodPrefab.Length; i++)
         {
-            GameObject go = GameObject.Find("Fridge/otkryt_pustoy/food/" + food[i]);
-            if (go != null)
+            if (foodPrefab[i] != null)
             {
-                go.SetActive(value: true);
-                int should = Random.Range(0, 2);
-                if (should == 1)
+                if (Random.Range(0, 2) == 1)
                 {
-                    go.SetActive(true);
-                    Vector3 temp = go.transform.position;
-                    go.transform.position = new Vector3(Random.Range(x[0], x[1]), y[Random.Range(0, y.Length - 1)], temp.z);
+                  for (int iter = 0; iter < Random.Range(0, 5); iter++)
+                  {
+                    GameObject tempFood = Instantiate(foodPrefab[i]);
+                    generatedFood.Add(tempFood);
+                    tempFood.SetActive(true);
+                    tempFood.transform.position = new Vector3(Random.Range(x[0], x[1]), y[Random.Range(0, y.Length - 1)], 6f);
+                  }
                 }
-                else
-                    go.SetActive(false);
             }
             else
             {
                 Debug.Log("but why?");
             }
         }
+    }
+
+    public static void ClearGeneratedFood()
+    {
+      for (int i = 0; i < generatedFood.Count; i++)
+      {
+        Destroy(generatedFood[i]);
+      }
+      generatedFood = new List<GameObject>();
     }
 }
